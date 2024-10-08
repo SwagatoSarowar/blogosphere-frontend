8a917c6dfcb99c9c981fb966683b975879e0f88b
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-10-09 00:22:09 +0600

    minor changes

--- src/pages/error/Unauthorized.tsx
+++ src/pages/error/Unauthorized.tsx
@@ -1,8 +1,4 @@
-import { useMetaTitle } from "../../hooks/useMetaTitle";
-
 export default function Unauthorized() {
-  useMetaTitle("Unauthorized");
-
   return (
     <div className="h-screen flex items-center justify-center text-primary dark:bg-boxdark-2 dark:text-bodydark">
       <div className="text-center flex flex-col gap-y-4">
